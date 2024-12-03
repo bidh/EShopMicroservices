06ba32920e5d6658643b7a0a1df2f09aa12fd29e
bidhgxe <bidh@gxe>
2024-12-03 22:17:38 +0100

    adding catalog api project

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, 
+        List<string> Category, 
+        string Description, 
+        string ImageFile, 
+        decimal Price) : IRequest<CreateProductResult>;
+    //needs to inherit IRequest to trigger CreateProductCommandHandler
+
+    public record CreateProductResult(Guid Id);
+
+    //IRequestHandler is a MediatR interface that is used to handle requests
+    //CreateProductCommand is the request that is being handled
+    //CreateProductResult is the result of the request
+    internal class CreateProductCommandHandler 
+        : IRequestHandler<CreateProductCommand,CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
