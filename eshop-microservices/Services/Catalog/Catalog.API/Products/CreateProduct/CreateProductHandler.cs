03622016f9520e0572b1624a542ef8fd0d7ccb28
bidhgxe <bidh@gxe>
2024-12-03 23:12:39 +0100

    implmenting product create endpoint

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -6,7 +7,7 @@ namespace Catalog.API.Products.CreateProduct
         List<string> Category, 
         string Description, 
         string ImageFile, 
-        decimal Price) : IRequest<CreateProductResult>;
+        decimal Price) : ICommand<CreateProductResult>;
     //needs to inherit IRequest to trigger CreateProductCommandHandler
 
     public record CreateProductResult(Guid Id);
@@ -15,11 +16,21 @@ namespace Catalog.API.Products.CreateProduct
     //CreateProductCommand is the request that is being handled
     //CreateProductResult is the result of the request
     internal class CreateProductCommandHandler 
-        : IRequestHandler<CreateProductCommand,CreateProductResult>
+        : ICommandHandler<CreateProductCommand,CreateProductResult>
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
