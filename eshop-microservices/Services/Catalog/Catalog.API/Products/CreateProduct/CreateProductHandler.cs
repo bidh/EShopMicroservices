dc51fbd29cb55551c49a7a896455c27f2ede2346
bidhgxe <bidh@gxe>
2024-12-03 22:41:34 +0100

    adding generic handlers

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -17,7 +17,7 @@ namespace Catalog.API.Products.CreateProduct
     internal class CreateProductCommandHandler 
         : IRequestHandler<CreateProductCommand,CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
