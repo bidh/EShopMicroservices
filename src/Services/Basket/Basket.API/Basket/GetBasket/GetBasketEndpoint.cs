b2a2afe20c6d0c2d0caa4e5a2d472def700ab65d
bidhgxe <bidh@gxe>
2024-12-14 21:41:40 +0100

    implementing get basket endpoint and handler

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿using Mapster;
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketEndpoint
+    //public record GetBasketRequest(string UserName);
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = sender.Send(new GetBasketQuery(userName));
+                var response = result.Adapt<GetBasketResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetBasketByUserName")
+            .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket by UserName")
+            .WithDescription("Get Basket by UserName");
+        }
     }
 }
