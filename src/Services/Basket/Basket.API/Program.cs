36eae1707c8593d60c58137ea94923802d83e37e
bidhgxe <bidh@gxe>
2024-12-17 21:37:10 +0100

    calling gRPC server(Discount service) from gRPC client (Basket service)

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,28 +1,26 @@
 using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Application services
 var assembly = typeof(Program).Assembly;
-
+builder.Services.AddCarter();
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssemblies(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-builder.Services.AddValidatorsFromAssembly(assembly);
 
-builder.Services.AddCarter();
-
+//Data services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
-    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);    
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
@@ -40,6 +38,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
     //options.InstanceName = "Basket_";
 });
 
+// gRPC services
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+});
+
+// Cross cutting services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 // Health checks are necessary to check the health of the service
 // and its dependencies. It is a good practice to add health checks
 // For this service, we are adding health checks for Postgres and Redis
