35cf793ca4f0feb5756d91890b387b4596d7ab72
bidhgxe <bidh@gxe>
2025-01-05 14:51:30 +0100

    create command created

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure
 {
@@ -10,14 +11,21 @@ namespace Ordering.Infrastructure
         {
             var connectionString = configuration.GetConnectionString("Database");
 
+            //adding auditable entity interceptor to update the audit fields when saving changes
             services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+
+            //adding dispatch domain events interceptor to dispatch domain events when saving changes
             services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
             // add services to the container.
             services.AddDbContext<ApplicationDbContext>((sp,options) =>
             {
+                //adding the interceptors to the db context
                 options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>());
                 options.UseSqlServer(connectionString);
             });
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
             return services;
         }
     }
