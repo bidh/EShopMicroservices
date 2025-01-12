56042f36a3fea48245f84f4f6762df13b9fa3e23
bidhgxe <bidh@gxe>
2025-01-12 16:43:39 +0100

    Event records added

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
