b681457f1234d29adbb1de00317f90483db7f14c
bidhgxe <bidh@gxe>
2025-01-12 16:54:01 +0100

    Message broker added

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MassTransit;
 public static class Extensions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    public static IServiceCollection AddMessageBroker(
+        this IServiceCollection services,
+        IConfiguration configuration, 
+        Assembly? assembly = null)
     {
+        // it is used to setup mass transit in dot net services collections
+        services.AddMassTransit(config =>
+        {
+            // it is used to set the kebab case endpoint name formatter
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            // coinfures the bus to use rabbit mq as a transport
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
         return services;
     }
 }
